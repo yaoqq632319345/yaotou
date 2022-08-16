5585c71368d01dfdf19a067aab8a360d14562b37
姚头 <632319345@qq.com>
2022-08-16 19:20:43 +0800

    Update index.js

--- utils/log/lib/index.js
+++ utils/log/lib/index.js
@@ -6,7 +6,6 @@ const npmlog = require('npmlog');
 npmlog.level = process.env.DEBUG_ ? process.env.DEBUG_ : 'info';
 function log() {
   npmlog.heading = '姚头';
-  npmlog.info('cli', 'test');
   npmlog.addLevel('success', 3000);
   npmlog.success('123123');
 }
