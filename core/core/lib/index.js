f5f456680664130d21cb3e6a4c6646e51cca830b
姚头 <632319345@qq.com>
2022-08-20 14:02:21 +0800

    检查node 版本

--- core/core/lib/index.js
+++ core/core/lib/index.js
@@ -1,11 +1,34 @@
 'use strict';
+const semver = require('semver');
+const colors = require('colors/safe');
 const pkg = require('../package.json');
 const log = require('@yaotou/log');
+
+const constant = require('./const');
 module.exports = core;
+
 function core(args) {
-  checkVersion();
+  try {
+    checkVersion();
+    checkNodeVersion();
+  } catch (error) {
+    log.error(error);
+  }
 }
 
 function checkVersion() {
   log.notice('cli version', pkg.version);
+}
+
+function checkNodeVersion() {
+  // 1. 获取当前node 版本
+  const current = process.version;
+  // 2. 对比最低版本
+  const lowest_version = constant.LOWEST_NODE_VERSION;
+  log.verbose('对比node version', '当前：', current, '最低：', lowest_version);
+  if (!semver.gte(current, lowest_version)) {
+    throw new Error(
+      colors.red(`需要安装 v${lowest_version} 以上版本的 Node.js`)
+    );
+  }
 }
