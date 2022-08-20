a88bf046823eb83875b54ef0c0e99bc782e3a9bc
姚头 <632319345@qq.com>
2022-08-20 14:52:36 +0800

    根据命令行参数 设置log level
    这里path-exists 不支持require，而且4.0版本也装不上，暂时跳过

--- core/core/lib/index.js
+++ core/core/lib/index.js
@@ -2,27 +2,47 @@
 const semver = require('semver');
 const colors = require('colors/safe');
 const userHome = require('user-home');
-const pathExists = require('path-exists');
+// const pathExists = require('path-exists');
 
 const log = require('@yaotou/log');
 const pkg = require('../package.json');
 const constant = require('./const');
 
+let args;
 module.exports = core;
 
 function core(args) {
+  checInputArgs();
   try {
     checkVersion();
     checkNodeVersion();
     checkRoot();
-    checkUserHome();
+    // checkUserHome();
   } catch (error) {
     log.error(error);
   }
 }
 
+// 检查debug 参数
+// npm: minimist
+function checInputArgs() {
+  const minimist = require('minimist');
+  args = minimist(process.argv.slice(2));
+  checkArgs();
+}
+
+function checkArgs() {
+  if (args.debug) {
+    process.env.LOG_LEVEL = 'verbose';
+  } else {
+    process.env.LOG_LEVEL = 'info';
+  }
+  log.level = process.env.LOG_LEVEL ? process.env.LOG_LEVEL : 'info';
+}
+
 // 检查用户主目录
 // npm: user-home & path-exists
+// path-exists 不支持require 4.0 版本也装不上，先不用了，跳过
 function checkUserHome() {
   if (!userHome || !pathExists(userHome)) {
     throw new Error(colors.red('当前登录用户主目录不存在'));
@@ -32,10 +52,10 @@ function checkUserHome() {
 // 检查root 登录账户,
 // npm: root-check
 function checkRoot() {
-  log.verbose(process.geteuid());
+  log.verbose('检查登录账户，前：', process.geteuid());
   const rootCheck = require('root-check');
   rootCheck();
-  log.verbose(process.geteuid());
+  log.verbose('检查登录账户，后：', process.geteuid());
 }
 
 // 项目版本
--- utils/log/lib/index.js
+++ utils/log/lib/index.js
@@ -3,5 +3,4 @@
 const npmlog = require('npmlog');
 npmlog.heading = 'yaotou';
 npmlog.headingStyle = { fg: 'red', bg: 'blue' };
-npmlog.level = process.env.LOG_LEVEL ? process.env.LOG_LEVEL : 'info';
 module.exports = npmlog;
