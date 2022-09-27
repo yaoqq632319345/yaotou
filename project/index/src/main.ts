8719b913ae427c46c6645854081c650bf1ce9d1c
姚头 <632319345@qq.com>
2022-09-27 18:15:44 +0800

    week9 4-1 引入antdvue

--- project/index/src/main.ts
+++ project/index/src/main.ts
@@ -1,6 +1,11 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import { Button, message } from 'ant-design-vue';
+import 'ant-design-vue/dist/antd.css';
 
-import './assets/main.css'
+import './assets/main.css';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(Button);
+app.mount('#app');
+app.config.globalProperties.$message = message;
