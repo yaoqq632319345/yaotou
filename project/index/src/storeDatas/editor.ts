8a12f34a86a9e498344bf2342100517794cb5e69
姚头 <632319345@qq.com>
2022-10-13 19:01:26 +0800

    week10 2-2 add LText component

--- project/index/src/storeDatas/editor.ts
+++ project/index/src/storeDatas/editor.ts
@@ -2,7 +2,15 @@ import type { ComponentData } from '@/stroeTypes';
 import { v4 as uuidv4 } from 'uuid';
 
 export const testComponents: ComponentData[] = [
-  { id: uuidv4(), name: 'l-text', props: { text: 'hello' } },
-  { id: uuidv4(), name: 'l-text', props: { text: 'hello2' } },
-  { id: uuidv4(), name: 'l-text', props: { text: 'hello3' } },
+  { id: uuidv4(), name: 'l-text', props: { text: 'hello', fontSize: '20px' } },
+  {
+    id: uuidv4(),
+    name: 'l-text',
+    props: { text: 'hello2', fontSize: '10px' },
+  },
+  {
+    id: uuidv4(),
+    name: 'l-text',
+    props: { text: 'hello3', fontSize: '15px' },
+  },
 ];
