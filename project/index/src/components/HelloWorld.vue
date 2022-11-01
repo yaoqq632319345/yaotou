6acacd2dbf38509b5319b2bea9d7baf4d892c9ef
姚头 <632319345@qq.com>
2022-11-01 11:20:48 +0800

    week11 3-1 init test environment

---
+++ project/index/src/components/HelloWorld.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>{{ msg }}</h1>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+export default defineComponent({
+  name: 'HelloWorld',
+  props: {
+    msg: String,
+  },
+});
+</script>
--- project/index/vite.config.ts
+++ project/index/vite.config.ts
@@ -1,6 +1,8 @@
+/// <reference types="vitest" />
+// 添加 vitest 的配置，与 vite 公用一份配置
 import { fileURLToPath, URL } from 'node:url';
-
 import { defineConfig } from 'vite';
+
 import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
@@ -10,6 +12,10 @@ export default defineConfig({
     jsxFactory: 'h',
     jsxFragment: 'Fragment',
     jsxInject: "import { h } from 'vue';",
+  },
+  test: {
+    // 全局api不用导入, vitest默认像test(), it()等方法需要导入
+    globals: true,
   },
   resolve: {
     alias: {
