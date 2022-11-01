8e9dad40346ec763621c5bc3793c61f22f437edd
姚头 <632319345@qq.com>
2022-11-01 13:46:35 +0800

    week11 3-3 test evnets

--- project/index/src/components/HelloWorld.vue
+++ project/index/src/components/HelloWorld.vue
@@ -1,6 +1,11 @@
 <template>
   <h1>{{ msg }}</h1>
-  <button @click="count++">{{ count }}</button>
+  <button @click="setCount">{{ count }}</button>
+  <input type="text" v-model="todo" />
+  <button class="addTodo" @click="addTodo">add</button>
+  <ul>
+    <li v-for="(todo, index) in todos" :key="index">{{ todo }}</li>
+  </ul>
   <hello msg="1234"></hello>
 </template>
 
@@ -15,10 +20,26 @@ export default defineComponent({
   props: {
     msg: String,
   },
-  setup() {
+  emits: ['send'],
+  setup(props, context) {
+    const todo = ref('');
+    const todos = ref<string[]>([]);
     const count = ref(1);
+    const setCount = () => {
+      count.value++;
+    };
+    const addTodo = () => {
+      if (todo.value) {
+        todos.value.push(todo.value);
+        context.emit('send', todo.value);
+      }
+    };
     return {
       count,
+      setCount,
+      todo,
+      todos,
+      addTodo,
     };
   },
 });
--- project/index/tests/unit/HelloWorld.test.ts
+++ project/index/tests/unit/HelloWorld.test.ts
@@ -23,4 +23,34 @@ describe('HelloWorld.vue', () => {
     expect(wrapper.props().msg).toBe(msg);
     expect(wrapper.findAllComponents(Hello)).toHaveLength(1);
   });
+
+  it('should update the count when clicking the button', async () => {
+    const msg = 'new message';
+    const wrapper = shallowMount(HelloWorld, {
+      props: { msg },
+    });
+    await wrapper.get('button').trigger('click');
+    expect(wrapper.get('button').text()).toBe('2');
+  });
+
+  it('should add todo when fill the input and click the add button', async () => {
+    const msg = 'new message';
+    const todoContent = 'buy milk';
+    const wrapper = shallowMount(HelloWorld, {
+      props: { msg },
+    });
+    await wrapper.get('input').setValue(todoContent);
+    expect(wrapper.get('input').element.value).toBe(todoContent);
+    await wrapper.get('.addTodo').trigger('click');
+    expect(wrapper.findAll('li')).toHaveLength(1);
+    expect(wrapper.get('li').text()).toBe(todoContent);
+    expect(wrapper.emitted()).toHaveProperty('send');
+    const events = wrapper.emitted('send');
+    expect(events![0]).toEqual([todoContent]);
+
+    // const events = wrapper.emitted();
+    // expect(events).toHaveProperty('send');
+    // const sendEvent = events.send;
+    // expect(sendEvent[0]).toEqual([todoContent]);
+  });
 });
