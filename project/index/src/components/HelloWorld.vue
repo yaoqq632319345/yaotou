22d53e36797535d6d6198241e58b269118c372f7
姚头 <632319345@qq.com>
2022-11-01 11:32:44 +0800

    week11 3-2 mount shallowMount

--- project/index/src/components/HelloWorld.vue
+++ project/index/src/components/HelloWorld.vue
@@ -1,13 +1,25 @@
 <template>
   <h1>{{ msg }}</h1>
+  <button @click="count++">{{ count }}</button>
+  <hello msg="1234"></hello>
 </template>
 
-<script>
-import { defineComponent } from 'vue';
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+import Hello from './Hello.vue';
 export default defineComponent({
   name: 'HelloWorld',
+  components: {
+    Hello,
+  },
   props: {
     msg: String,
+  },
+  setup() {
+    const count = ref(1);
+    return {
+      count,
+    };
   },
 });
 </script>
--- project/index/tests/unit/HelloWorld.test.ts
+++ project/index/tests/unit/HelloWorld.test.ts
@@ -1,12 +1,26 @@
-import { shallowMount } from '@vue/test-utils';
+import { shallowMount, mount } from '@vue/test-utils';
 import HelloWorld from '@/components/HelloWorld.vue';
+import Hello from '@/components/Hello.vue';
+import TemplateList from '@/components/TemplateList.vue';
 
 describe('HelloWorld.vue', () => {
   it('renders props.msg when passed', () => {
     const msg = 'new message';
-    const wrapper = shallowMount(HelloWorld, {
+    const shallowWrapper = shallowMount(HelloWorld, {
       props: { msg },
     });
-    expect(wrapper.text()).toMatch(msg);
+
+    expect(shallowWrapper.html()).toMatchSnapshot();
+    expect(shallowWrapper.props().msg).toBe(msg);
+    expect(shallowWrapper.findAllComponents(Hello)).toHaveLength(1);
+    expect(shallowWrapper.findAllComponents(TemplateList)).toHaveLength(0);
+
+    const wrapper = mount(HelloWorld, {
+      props: { msg },
+    });
+
+    expect(wrapper.html()).toMatchSnapshot();
+    expect(wrapper.props().msg).toBe(msg);
+    expect(wrapper.findAllComponents(Hello)).toHaveLength(1);
   });
 });
