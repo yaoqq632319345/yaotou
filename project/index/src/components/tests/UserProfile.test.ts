a422505f246f93270c2dfdf968f2f8fa04e6b523
姚头 <632319345@qq.com>
2022-11-02 17:02:37 +0800

    week11 4-2 mock antd message and store

--- project/index/src/components/tests/UserProfile.test.ts
+++ project/index/src/components/tests/UserProfile.test.ts
@@ -1,13 +1,21 @@
 import { mount, type VueWrapper } from '@vue/test-utils';
 import UserProfile from '@/components/UserProfile.vue';
 import { afterAll, beforeAll, vi, describe, it, expect } from 'vitest';
+import { message } from 'ant-design-vue';
+import store from '@/storeVuex';
+
 let wrapper: VueWrapper<any>;
+
 beforeAll(() => {
-  vi.mock('vuex');
   vi.mock('vue-router');
+  vi.mock('ant-design-vue', () => ({
+    message: {
+      success: vi.fn(),
+    },
+  }));
 });
 afterAll(() => {
-  vi.resetModules();
+  vi.clearAllMocks();
 });
 const mockComponent = {
   template: '<div><slot></slot></div>',
@@ -30,12 +38,16 @@ describe('UserProfile component', () => {
         user: { isLogin: false },
       },
       global: {
+        provide: { store },
         components: globalComponents,
       },
     });
   });
-  it('should render login button when login is false', () => {
+  it('should render login button when login is false', async () => {
     expect(wrapper.get('div').text()).toBe('登录');
+    await wrapper.get('div').trigger('click');
+    expect(message.success).toHaveBeenCalled();
+    expect(store.state.user.userName).toBe('viking');
   });
   it('should render username when login is true', async () => {
     await wrapper.setProps({
