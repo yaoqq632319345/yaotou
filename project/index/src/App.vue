5a0cac6b3131de7be411ac406b16200315f643e4
姚头 <632319345@qq.com>
2022-11-10 11:29:40 +0800

    week12 2-2 test upload

--- project/index/src/App.vue
+++ project/index/src/App.vue
@@ -1,6 +1,25 @@
 <script setup lang="ts">
 import { computed } from 'vue';
 import { useRoute } from 'vue-router';
+import axios from 'axios';
+const handleFileChange = (e: Event) => {
+  const target = e.target as HTMLInputElement;
+  const files = target.files;
+  if (files) {
+    const uploadedFile = files[0];
+    const formData = new FormData();
+    formData.append(uploadedFile.name, uploadedFile);
+    axios
+      .post('http://local.test:7001/api/upload', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      })
+      .then((resp) => {
+        console.log(resp.data);
+      });
+  }
+};
 
 const route = useRoute();
 const withHeader = computed(() => route.meta.withHeader);
@@ -8,6 +27,11 @@ const withHeader = computed(() => route.meta.withHeader);
 
 <template>
   <div class="app-container">
+    <form method="post" action="http://local.test:7001/api/upload">
+      <input type="file" name="file" @change="handleFileChange" />
+      <input type="text" name="test" />
+      <button type="submit">Submit</button>
+    </form>
     <router-view />
   </div>
 </template>
