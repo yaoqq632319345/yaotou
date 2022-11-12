2e1ff816cde8a3fb5e766f6f4e36ef23094bb534
姚头 <632319345@qq.com>
2022-11-12 10:06:33 +0800

    week12 3-2 implement uploader test case

--- project/index/src/components/Uploader.vue
+++ project/index/src/components/Uploader.vue
@@ -1,9 +1,7 @@
 <template>
   <div class="file-upload">
-    <button @click="triggerUpload">
-      <span v-if="fileStatus === 'loading'">正在上传</span>
-      <span v-else-if="fileStatus === 'success'">上传成功</span>
-      <span v-else-if="fileStatus === 'error'">上传失败</span>
+    <button @click="triggerUpload" :disabled="isUploading">
+      <span v-if="isUploading">正在上传</span>
       <span v-else>点击上传</span>
     </button>
     <input
@@ -12,13 +10,47 @@
       :style="{ display: 'none' }"
       @change="handleFileChange"
     />
+    <ul class="upload-list">
+      <li
+        :class="`uploaded-file upload-${file.status}`"
+        v-for="file in uploadedFiles"
+        :key="file.uid"
+      >
+        <!-- <span v-if="file.status === 'loading'" class="file-icon"
+          ><LoadingOutlined
+        /></span>
+        <span v-else class="file-icon"><FileOutlined /></span> -->
+        <span class="filename">{{ file.name }}</span>
+        <span class="delete-icon" @click="removeFile(file.uid)">
+          <DeleteOutlined />
+        </span>
+      </li>
+    </ul>
   </div>
 </template>
 <script lang="ts">
-import { defineComponent, ref } from 'vue';
+import { defineComponent, ref, computed, reactive } from 'vue';
 import axios from 'axios';
+import { v4 as uuidv4 } from 'uuid';
+import {
+  DeleteOutlined,
+  LoadingOutlined,
+  FileOutlined,
+} from '@ant-design/icons-vue';
 type UploadStaus = 'ready' | 'loading' | 'success' | 'error';
+export interface UploadFile {
+  uid: string;
+  size: number;
+  name: string;
+  status: UploadStaus;
+  raw: File;
+}
 export default defineComponent({
+  components: {
+    DeleteOutlined,
+    LoadingOutlined,
+    FileOutlined,
+  },
   props: {
     action: {
       type: String,
@@ -27,7 +59,15 @@ export default defineComponent({
   },
   setup(props) {
     const fileInput = ref<null | HTMLInputElement>(null);
-    const fileStatus = ref<UploadStaus>('ready');
+    const uploadedFiles = ref<UploadFile[]>([]);
+    const isUploading = computed(() => {
+      return uploadedFiles.value.some((file) => file.status === 'loading');
+    });
+    const removeFile = (id: string) => {
+      uploadedFiles.value = uploadedFiles.value.filter(
+        (file) => file.uid !== id
+      );
+    };
     const triggerUpload = () => {
       if (fileInput.value) {
         fileInput.value.click();
@@ -40,7 +80,14 @@ export default defineComponent({
         const uploadedFile = files[0];
         const formData = new FormData();
         formData.append(uploadedFile.name, uploadedFile);
-        fileStatus.value = 'loading';
+        const fileObj = reactive<UploadFile>({
+          uid: uuidv4(),
+          size: uploadedFile.size,
+          name: uploadedFile.name,
+          status: 'loading',
+          raw: uploadedFile,
+        });
+        uploadedFiles.value.push(fileObj);
         axios
           .post(props.action, formData, {
             headers: {
@@ -48,18 +95,25 @@ export default defineComponent({
             },
           })
           .then((resp) => {
-            fileStatus.value = 'success';
+            fileObj.status = 'success';
           })
           .catch(() => {
-            fileStatus.value = 'error';
+            fileObj.status = 'error';
+          })
+          .finally(() => {
+            if (fileInput.value) {
+              fileInput.value.value = '';
+            }
           });
       }
     };
     return {
       fileInput,
       triggerUpload,
-      fileStatus,
       handleFileChange,
+      isUploading,
+      uploadedFiles,
+      removeFile,
     };
   },
 });
--- project/index/src/components/tests/Uploader.test.ts
+++ project/index/src/components/tests/Uploader.test.ts
@@ -5,17 +5,17 @@ import {
   describe,
   it,
   expect,
-  beforeAll,
   type SpyInstance,
   vi,
   afterEach,
+  beforeEach,
 } from 'vitest';
 
 describe('Uploader Component', () => {
   const actionURL = 'xxxxxxxxxxxxxx';
   let viSpyAxios: SpyInstance<any>;
   let wrapper: VueWrapper<any>;
-  beforeAll(async () => {
+  beforeEach(async () => {
     viSpyAxios = vi.spyOn(axios, 'post');
     wrapper = await shallowMount(Uploader, {
       props: {
@@ -24,6 +24,7 @@ describe('Uploader Component', () => {
     });
   });
   afterEach(() => {
+    wrapper.unmount();
     // 重置调用次数等等..., 可以在配置中配置clearMocks
     viSpyAxios.mockReset();
   });
@@ -32,7 +33,7 @@ describe('Uploader Component', () => {
     expect(wrapper.get('button span').text()).toBe('点击上传');
     expect(wrapper.get('input').isVisible()).toBeFalsy();
   });
-  it.only('upload process should works fine', async () => {
+  it('upload process should works fine', async () => {
     viSpyAxios.mockResolvedValueOnce({ status: 'success' });
     const fileInput = wrapper.get('input').element as HTMLInputElement;
     const testFile = new File(['xyz'], 'test.png', { type: 'image/png' });
@@ -53,11 +54,11 @@ describe('Uploader Component', () => {
 
     expect(wrapper.get('button span').text()).toBe('正在上传');
     // button 为 disabled
-    expect(wrapper.get('button').attributes('disabled')).toBe('');
+    expect(wrapper.get('button').attributes()).toHaveProperty('disabled');
     // 列表长度修改，并且有正确的 class
     expect(wrapper.findAll('li').length).toBe(1);
     const firstItem = wrapper.get('li:first-child');
-    expect(firstItem.classes()).toContain('uploading');
+    expect(firstItem.classes()).toContain('upload-loading');
     await flushPromises();
     expect(wrapper.get('button span').text()).toBe('点击上传');
     // 有正确的 class，并且文件名称相对应
@@ -66,6 +67,15 @@ describe('Uploader Component', () => {
   });
   it('should return error text when post is rejected', async () => {
     viSpyAxios.mockRejectedValueOnce({ error: 'error' });
+    // 每个测试用例互不影响
+    const fileInput = wrapper.get('input').element as HTMLInputElement;
+    const testFile = new File(['xyz'], 'test.png', { type: 'image/png' });
+    const files = [testFile];
+    // input files 是只读的，所以只能用这种方式赋值
+    Object.defineProperty(fileInput, 'files', {
+      value: files,
+      writable: false,
+    });
     await wrapper.get('input').trigger('change');
     expect(axios.post).toHaveBeenCalledTimes(1);
     // 未知参数个数，判断部分参数
@@ -74,15 +84,16 @@ describe('Uploader Component', () => {
     expect(viSpyAxios.mock.lastCall).toEqual(
       expect.arrayContaining([actionURL])
     );
+    expect(viSpyAxios.mock.lastCall).toContain(actionURL);
     expect(wrapper.get('button span').text()).toBe('正在上传');
     await flushPromises();
-    expect(wrapper.get('button span').text()).toBe('上传失败');
+    expect(wrapper.get('button span').text()).toBe('点击上传');
     // 列表长度增加，并且列表的最后一项有正确的 class 名
-    expect(wrapper.findAll('li').length).toBe(2);
+    expect(wrapper.findAll('li').length).toBe(1);
     const lastItem = wrapper.get('li:last-child');
     expect(lastItem.classes()).toContain('upload-error');
     // 点击列表中右侧的 button，可以删除这一项
     await lastItem.get('.delete-icon').trigger('click');
-    expect(wrapper.findAll('li').length).toBe(1);
+    expect(wrapper.findAll('li').length).toBe(0);
   });
 });
