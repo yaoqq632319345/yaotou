8418665be08cc27fc48ce1428c4012aaeaecdd72
姚头 <632319345@qq.com>
2022-11-22 13:46:28 +0800

    ✨ feat(week13 3-1): components init

---
+++ components/index/src/App.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="app-container"></div>
+</template>
+
+<style scoped></style>
---
+++ components/index/vite.config.ts
@@ -0,0 +1,15 @@
+/// <reference types="vitest" />
+import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from 'vite';
+
+import vue from '@vitejs/plugin-vue';
+
+export default defineConfig({
+  plugins: [vue()],
+  esbuild: {},
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+});
